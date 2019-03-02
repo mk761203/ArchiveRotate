4cb0a8aa0859632d9cbd60ea3bdf50d4a06a6eaf
Michal Kowalewski <michal@x-net.pl>
2019-03-02 09:02:01 +0100

    support for required parameters improved remove_files method

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ArchiveRotate
-Rotate backup files/folders to keep gives free space on disk.
+Rotate backup files/folders to keep expected free space on disk.
+Ordering is based on alphabetical order, so it works for names
+e.g. with timestamp at the beginning.
+Files/folders with the 'smallest' names will be removed as the first - 
+e.g. file 'a' will be removed before file 'b' or file '0001' will be removed before file '0111'.
--- py2.6.5/main.py
+++ py2.6.5/main.py
@@ -1,29 +1,37 @@
 import os
-import shutil
 import sys
 import datetime
 from optparse import OptionParser
 
+# consts
+_EXIT_STATUS_OK = 0
+_EXIT_STATUS_LACK_OF_PARAMS = 1
+_EXIT_STATUS_PATH_TOO_SHORT = 10
+_EXIT_STATUS_DIR_NOT_FOUND = 11
+
 # config
 parser = OptionParser()
-parser.add_option("-p", "--path", type="string", help="location where backup files/folders are stored")
-parser.add_option("-m", "--min-disk-space", type="long", help="minimum required disk space in bytes")
+parser.add_option("-p", "--path", type="string", help="[required] location where backup files/folders are stored")
+parser.add_option("-m", "--min-disk-space", type="long", help="[required] minimum required disk space in bytes")
 parser.add_option("-e", "--eval-min-disk-space", type="string",
                   help="expression which evaluate to minimum required disk space in bytes e.g. 30 * 10 ** 9")
-parser.add_option("-f", "--max-files-to-remove", type="long", help="maximum number of files to remove")
-parser.add_option("-l", "--min-path-length", type="long", default=1, help="minimum path length (defense)")
+parser.add_option("-n", "--max-files-to-remove", type="long", default=1, help="maximum number of files to remove")
+parser.add_option("-d", "--min-path-length", type="long", default=2, help="minimum path length (defense)")
+parser.add_option("-l", "--log-directory-content", action='store_true', default=False,
+                  help="write to log directory content before and after rotate")
 (opts, args) = parser.parse_args(sys.argv)
 _location = opts.path
 _min_disk_space_bytes = eval(opts.eval_min_disk_space) if opts.eval_min_disk_space else opts.min_disk_space
 _max_files_to_remove = opts.max_files_to_remove
 _min_path_length = opts.min_path_length
-_remove_function = shutil.rmtree  # for directories
-# _remove_function = os.remove  # for files
+_log_directory_content = opts.log_directory_content
+# import shutil; _remove_function = shutil.rmtree  # for directories
+_remove_function = os.remove  # for files
 
-# consts
-_EXIT_STATUS_OK = 0
-_EXIT_STATUS_PATH_TOO_SHORT = 1
-_EXIT_STATUS_DIRECTORY_NOT_FOUND = 2
+if None in [_location, _min_disk_space_bytes]:
+    print('\nLack of required parameter(s)./\n')
+    parser.print_help()
+    sys.exit(_EXIT_STATUS_LACK_OF_PARAMS)
 
 
 class ArchiveRotate:
@@ -33,7 +41,8 @@ class ArchiveRotate:
 
     def ls(self, _dir, comment=None):
         files = os.listdir(_dir)
-        self.log += ['[{}] directory: {}, content: {}'.format(comment, _dir, files)]
+        if _log_directory_content:
+            self.log += ['[{}] file: {}, content: {}'.format(comment, _dir, files)]
 
     @staticmethod
     def bytes_free_on_drive():
@@ -57,12 +66,12 @@ class ArchiveRotate:
     def check_directory(self):
         if not os.path.exists(_location):
             self.log += ['file: {} not found'.format(_location)]
-            self.end(_EXIT_STATUS_DIRECTORY_NOT_FOUND)
+            self.end(_EXIT_STATUS_DIR_NOT_FOUND)
 
     def remove_files(self):
         self.ls(_location, 'before')
         no_of_files_to_remove = _max_files_to_remove
-        while no_of_files_to_remove > 0:
+        while no_of_files_to_remove > 0 and self.bytes_free_on_drive() < _min_disk_space_bytes:
             self.remove_first_file()
             no_of_files_to_remove -= 1
         self.ls(_location, 'after')
@@ -77,17 +86,22 @@ class ArchiveRotate:
                 print(entry)
         sys.exit(_exit_status)
 
+    def get_free_space(self):
+        bytes_free = self.bytes_free_on_drive()
+        self.log += ['drive: {}, bytes free: {}'.format(_location, bytes_free)]
+        return bytes_free
+
     def execute(self):
         self.begin()
         self.check_directory()
-        bytes_free = self.bytes_free_on_drive()
-        self.log += ['drive: {}, bytes free: {}'.format(_location, bytes_free)]
-        if bytes_free < _min_disk_space_bytes:
+        if self.get_free_space() < _min_disk_space_bytes:
+            self.log += ['not enough disk space, rotate needed']
             self.print_log = True  # to print log only if something is removed or failure happen
             self.remove_files()
         else:
             self.log += ["no action needed"]
-            self.end(_EXIT_STATUS_OK)
+        self.get_free_space()
+        self.end(_EXIT_STATUS_OK)
 
 
 if __name__ == "__main__":
